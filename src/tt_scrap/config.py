babd6f3cb29297c76a5aa2c9e22300564d0e8a3d
Kyryl Andreiev <karilaa@karilaa.dev>
2026-08-09 09:55:21 +0000

    Release v1.1.0

--- src/tt_scrap/config.py
+++ src/tt_scrap/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     )
 
     app_name: str = "tt-scrap"
-    app_version: str = "1.0.0"
+    app_version: str = "1.1.0"
     environment: str = "production"
     log_level: str = "INFO"
     log_format: str = "console"
